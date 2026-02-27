05760b26ca440c048b712eeffc5faebefc487ddc
DiegoRRQ <finnbusinessmail@gmail.com>
2026-02-27 19:55:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,11 @@ import discord
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True  # needed to read messages
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-# --- Your trigger words and responses ---
-KEY_TRIGGERS = ["key", "keys"]  # case-insensitive check below
+KEY_TRIGGERS = ["key", "keys"]
 KEY_RESPONSE = "All the keys for Diego's scripts can be found in https://discord.com/channels/1458595915463000147/1459979110481793188"
 
 @client.event
@@ -17,11 +16,8 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return  # ignore the bot's own messages
-
+        return
     msg = message.content.lower()
-
-    # Check if any trigger word appears in the message
     if any(word in msg.split() for word in KEY_TRIGGERS):
         await message.channel.send(KEY_RESPONSE)
 
